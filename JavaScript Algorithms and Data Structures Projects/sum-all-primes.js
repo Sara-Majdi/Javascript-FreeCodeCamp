d890aadade22c8107f6554ec537c0a3167c538dd
Sara Majdi <123373205+Sara-Majdi@users.noreply.github.com>
2023-08-18 22:25:28 +0800

    Delete sum-all-primes.js

--- JavaScript Algorithms and Data Structures Projects/sum-all-primes.js
+++
@@ -1,9 +0,0 @@
-function sumPrimes(num) {
-  let primeNum = [];
-  for (let i =2; i <= num; i++){
-    if (primeNum.every((primeNum) => i % primeNum !== 0))primeNum.push(i);
-  }
-  return primeNum.reduce((sum, primeNum) => sum + primeNum, 0);
-}
-
-sumPrimes(10);
